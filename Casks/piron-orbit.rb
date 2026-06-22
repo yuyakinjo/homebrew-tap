a5ddbd90b7bf3fed74e9e051fed2c19727c34ba4
kinjo <kinjo.bobchin@gmail.com>
2026-06-22 23:41:04 +0900

    Add Piron Orbit cask

---
+++ Casks/piron-orbit.rb
@@ -0,0 +1,21 @@
+cask "piron-orbit" do
+  version "2026.6.22"
+  sha256 "cfc152cbd1c65ce0110937f536a51f5c1f9d683466f2bf5eb6783d399f3158f1"
+
+  url "https://github.com/yuyakinjo/homebrew-tap/releases/download/piron-orbit-v#{version}/Piron-Orbit_#{version}_aarch64.zip",
+      verified: "github.com/yuyakinjo/homebrew-tap/"
+  name "Piron Orbit"
+  desc "Git workflow controller"
+  homepage "https://github.com/yuyakinjo/homebrew-tap"
+
+  depends_on arch: :arm64
+
+  app "Piron Orbit.app"
+
+  zap trash: [
+    "~/Library/Application Support/Piron Orbit",
+    "~/Library/Caches/com.yuyakinjo.piron-orbit",
+    "~/Library/Preferences/com.yuyakinjo.piron-orbit.plist",
+    "~/Library/Saved Application State/com.yuyakinjo.piron-orbit.savedState",
+  ]
+end
