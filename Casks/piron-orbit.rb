4325210b019d9464e6de37025ecb90e4db7d4d46
kinjo <kinjo.bobchin@gmail.com>
2026-06-23 00:59:04 +0900

    Update Piron Orbit to 2026.6.23

--- Casks/piron-orbit.rb
+++ Casks/piron-orbit.rb
@@ -1,6 +1,6 @@
 cask "piron-orbit" do
-  version "2026.6.22"
-  sha256 "cfc152cbd1c65ce0110937f536a51f5c1f9d683466f2bf5eb6783d399f3158f1"
+  version "2026.6.23"
+  sha256 "41dfe414e3941c968de9bfd1139bb93814c2c37e6e66b2a08f92fdb102598dea"
 
   url "https://github.com/yuyakinjo/homebrew-tap/releases/download/piron-orbit-v#{version}/Piron-Orbit_#{version}_aarch64.zip",
       verified: "github.com/yuyakinjo/homebrew-tap/"
