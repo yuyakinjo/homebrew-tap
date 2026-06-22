3d0252189dd9e270824157e620ead92f6d25e1af
kinjo <kinjo.bobchin@gmail.com>
2026-06-23 01:00:41 +0900

    Fix Piron Orbit cask audit

--- Casks/piron-orbit.rb
+++ Casks/piron-orbit.rb
@@ -2,8 +2,7 @@ cask "piron-orbit" do
   version "2026.6.23"
   sha256 "41dfe414e3941c968de9bfd1139bb93814c2c37e6e66b2a08f92fdb102598dea"
 
-  url "https://github.com/yuyakinjo/homebrew-tap/releases/download/piron-orbit-v#{version}/Piron-Orbit_#{version}_aarch64.zip",
-      verified: "github.com/yuyakinjo/homebrew-tap/"
+  url "https://github.com/yuyakinjo/homebrew-tap/releases/download/piron-orbit-v#{version}/Piron-Orbit_#{version}_aarch64.zip"
   name "Piron Orbit"
   desc "Git workflow controller"
   homepage "https://github.com/yuyakinjo/homebrew-tap"
